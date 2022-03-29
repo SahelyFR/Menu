67bef0f419168a32aacdc0ae61637ded0adfd21f
SahelyFR <damien.telliez@gmail.com>
2022-03-29 17:11:17 +0200

    feat: :sparkles: recipe's season management added

--- Code.js
+++ Code.js
@@ -1,12 +1,23 @@
+var mySeason = 0;
+
 const SHEET = SpreadsheetApp.getActiveSpreadsheet();
 
 const RECETTES_TAB = SHEET.getSheetByName('Recettes');
+const PARAMS_TAB = SHEET.getSheetByName('Params');
 const RECETTES = RECETTES_TAB.getDataRange().getValues();
 
 const MENU_TAB = SHEET.getSheetByName('Menu');
 const MENU = MENU_TAB.getDataRange().getValues();
 
-//const cache = CACHE_TAB.getRange(2, 1, 1, 3);
+const MY_SEASON = PARAMS_TAB.getRange(1,2);
+
+const PRINTEMPS = 80;
+const ETE = 172;
+const AUTOMNE = 264;
+const HIVER = 359;
+
+//PropertiesService.getScriptProperties().setProperty('SEASON', '');
+
 
 function shuffle(array) {
   var currentIndex = array.length,  randomIndex;
@@ -26,19 +37,42 @@ function shuffle(array) {
 
 function retrieveRecipes(nb){
     const recetteList = [];
-    RECETTES.forEach(function(row) {
+    var seasonRecipes = RECETTES.filter(filtreSaison);
+    seasonRecipes.forEach(function(row) {
         if(row[0] != ''){
           recetteList.push(row);
         }
     });
     recetteList.splice(0, 1);
-    shuffle(recetteList);
-    recetteList.splice(0, recetteList.length-nb)
-    return recetteList;
+    return shuffle(recetteList);
 }
 
-/* eslint-disable no-unused-vars */
-// eslint-disable-next-line require-jsdoc
+function determineSeason(){
+  var currentDay = Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "DD");
+  //var SEASON = "";
+  if(currentDay >= PRINTEMPS && currentDay < ETE){
+    mySeason = 1;
+  }
+  if(currentDay >= ETE && currentDay < AUTOMNE){
+    mySeason = 2;
+  }
+  if(currentDay >= AUTOMNE && currentDay < HIVER){
+    mySeason = 3;
+  }
+  if(currentDay >= HIVER || currentDay < PRINTEMPS){
+    mySeason = 4;
+  }
+  MY_SEASON.setValue(mySeason);
+}
+
+function getSeason(){
+  return MY_SEASON.getValue();
+}
+
+function filtreSaison(arr){ 
+  return arr[MY_SEASON.getValue()+1] === true;
+}
+
 function doGet() {
     'use strict';
     const html = HtmlService.createTemplateFromFile('Front').evaluate();
