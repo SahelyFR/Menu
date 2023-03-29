f4e9ef83b86b04b4b1322c2361693a98a44ddbaf
SahelyFR <damien.telliez@gmail.com>
2023-03-29 17:03:28 +0200

    - update doc - remove unused sheet tabs declaration

--- Code.js
+++ Code.js
@@ -1,26 +1,18 @@
-var mySeason = 0;
+let currentSeason = 0;
 
 const SHEET = SpreadsheetApp.getActiveSpreadsheet();
-
 const RECETTES_TAB = SHEET.getSheetByName('Recettes');
-const PARAMS_TAB = SHEET.getSheetByName('Params');
 const RECETTES = RECETTES_TAB.getDataRange().getValues();
-
-const MENU_TAB = SHEET.getSheetByName('Menu');
-const MENU = MENU_TAB.getDataRange().getValues();
-
-const MY_SEASON = PARAMS_TAB.getRange(1,2);
 
 const PRINTEMPS = 80;
 const ETE = 172;
 const AUTOMNE = 264;
 const HIVER = 359;
 
-//PropertiesService.getScriptProperties().setProperty('SEASON', '');
-
+const MY_SEASON = parseInt(PropertiesService.getScriptProperties().getProperty('season'));
 
 function shuffle(array) {
-  var currentIndex = array.length,  randomIndex;
+  let currentIndex = array.length,  randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
@@ -32,13 +24,14 @@ function shuffle(array) {
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex], array[currentIndex]];
   }
+  Logger.log(array);
   return array;
 }
 
 function retrieveRecipes(nb){
     const recetteList = [];
-    var seasonRecipes = RECETTES.filter(filtreSaison);
-    //Logger.log(seasonRecipes)
+    let seasonRecipes = RECETTES.filter(filtreSaison);
+    
     seasonRecipes.forEach(function(row) {
         if(row[0] != ''){
           recetteList.push(row);
@@ -46,35 +39,35 @@ function retrieveRecipes(nb){
     });
     
     recetteList.splice(0, 1);
-    Logger.log(recetteList)
+    
     return shuffle(recetteList);
 }
 
 function determineSeason(){
-  var currentDay = Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "DD");
-  //var SEASON = "";
+  let currentDay = Utilities.formatDate(new Date(), Session.getScriptTimeZone(), "DD");
+  
   if(currentDay >= PRINTEMPS && currentDay < ETE){
-    mySeason = 1;
+    currentSeason = 1;
   }
   if(currentDay >= ETE && currentDay < AUTOMNE){
-    mySeason = 2;
+    currentSeason = 2;
   }
   if(currentDay >= AUTOMNE && currentDay < HIVER){
-    mySeason = 3;
+    currentSeason = 3;
   }
   if(currentDay >= HIVER || currentDay < PRINTEMPS){
-    mySeason = 4;
+    currentSeason = 4;
   }
-  MY_SEASON.setValue(mySeason);
+  PropertiesService.getScriptProperties().setProperty('season', currentSeason);
 }
 
 function getSeason(){
-  return MY_SEASON.getValue();
+  return MY_SEASON;
 }
 
 function filtreSaison(arr){ 
   // +2 for 2 columns before start of seasons in recettes tab
-  return arr[MY_SEASON.getValue()+2] === true;
+  return arr[MY_SEASON+2] === true;
 }
 
 function doGet() {
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Menu
 
+## Goal
 This little project is created to auto-generate a menu, for a complete week or selected days, depending on recipe's season.
+
+## How-to
+### Use the script on your side
+You can use [this Google Sheet](https://docs.google.com/spreadsheets/d/1fhdx95YJpTuFtM0gy_oX2BLcXxQ_qQsDS4q5dKjrElM/edit?usp=sharing) as template to add your recipes and make them available for this script.  
+Once you've duplicated the above template sheet, you can fill it with your own recipes, with link to instructions, picture and the suitable season.  
+:warning: Think to update attached script with the one contained in this repository. :warning: (instructions below)
+
+### Copy this repository's code to your own Google sheet
+The easiest way, if you just want to use it is to simply copy/paste code contained in Code.js and Front.html in the equivalent files of the template sheet you've duplicated on your side.  
+To do so, when on the template sheet, go to the "Extensions > Apps Scripts" menu item.
+
+### For those of you who may want to update the script
+You can use your loved IDE to develop with Apps Scripts. You will just need to install CLASP (Apps Script CLI) on your terminal.  
+Everything is then summarized and explained on [Google Apps Script documentation](https://developers.google.com/apps-script/guides/clasp)
